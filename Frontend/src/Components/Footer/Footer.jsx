c4da7b322b979a1272344d5802b89465b178506d
ezecon <nationhexus@gmail.com>
2024-08-16 11:58:45 +0600

    cc

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="bg-black w-full p-6 md:p-10">
       <div className="flex flex-col md:flex-row items-center justify-around">
-        <div className="text-center md:text-left mb-4 md:mb-0 footer-left-1">
+        <div className="text-center md:text-left mb-4 md:mb-0 hero-left-1">
           <h1 className="text-white font-bold text-4xl">HALAL BRO</h1>
           <div className="flex justify-center md:justify-start gap-4 mt-2">
             <p className="text-white montserrat-alternates-light gradient">About</p>
@@ -12,7 +12,7 @@ export default function Footer() {
             <p className="text-white montserrat-alternates-light gradient">Address</p>
           </div>
         </div>
-        <div className="w-full md:w-1/2 lg:w-1/3  footer-right-1">
+        <div className="w-full md:w-1/2 lg:w-1/3  autoShow">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.974846815392!2d91.18006207598292!3d23.46137177886871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37547f03755c1bd5%3A0xfbac0e32109b29ec!2sHalal%20Brothers!5e0!3m2!1sen!2sbd!4v1723728029976!5m2!1sen!2sbd"
             className="w-full h-48 md:h-64 rounded-md shadow-lg"
--- Frontend/src/Components/Gallery/Gallery.jsx
+++ Frontend/src/Components/Gallery/Gallery.jsx
@@ -1,31 +1,42 @@
-
-
 export default function Gallery() {
   return (
-    <div className="my-20 autoShow">
-         <h1 className="checkcursor heading new-amsterdam-regular text-6xl text-center m-10 ">
+    <div className="my-24 autoShow">
+      <h1 className="checkcursor heading new-amsterdam-regular text-6xl text-center">
         Gallery
       </h1>
 
-      <div className="gap-3 relative w-full h-[500px] py-5 flex justify-center image-slider items-center">
-  <div className="slider-wrapper">
-    <img src="1_.jpg" alt="Image 1" className="slider-image"/>
-    <img src="2.jpg" alt="Image 1" className="slider-image"/>
-    <img src="3.jpg" alt="Image 1" className="slider-image"/>
-    <img src="4.jpg" alt="Image 1" className="slider-image"/>
-    <img src="5.jpg" alt="Image 1" className="slider-image"/>
-    <img src="6.jpg" alt="Image 1" className="slider-image"/>
-    <img src="7.jpg" alt="Image 1" className="slider-image"/>
-    <img src="8.jpg" alt="Image 1" className="slider-image"/>
-    <img src="9.jpg" alt="Image 1" className="slider-image"/>
-    <img src="10.jpg" alt="Image 1" className="slider-image"/>
-    <img src="11.jpg" alt="Image 1" className="slider-image"/>
-    <img src="12.jpg" alt="Image 1" className="slider-image"/>
-    <img src="13.jpg" alt="Image 1" className="slider-image"/>
-  </div>
-</div>
-
-
+      <div className="gap-3 relative w-full h-[500px] flex justify-center image-slider items-center">
+        <div className="slider-wrapper">
+          <img src="1_.jpg" alt="Image 1" className="slider-image"/>
+          <img src="2.jpg" alt="Image 2" className="slider-image"/>
+          <img src="3.jpg" alt="Image 3" className="slider-image"/>
+          <img src="4.jpg" alt="Image 4" className="slider-image"/>
+          <img src="5.jpg" alt="Image 5" className="slider-image"/>
+          <img src="6.jpg" alt="Image 6" className="slider-image"/>
+          <img src="7.jpg" alt="Image 7" className="slider-image"/>
+          <img src="8.jpg" alt="Image 8" className="slider-image"/>
+          <img src="9.jpg" alt="Image 9" className="slider-image"/>
+          <img src="10.jpg" alt="Image 10" className="slider-image"/>
+          <img src="11.jpg" alt="Image 11" className="slider-image"/>
+          <img src="12.jpg" alt="Image 12" className="slider-image"/>
+          <img src="13.jpg" alt="Image 13" className="slider-image"/>
+          
+          {/* Duplicate images for seamless looping */}
+          <img src="1_.jpg" alt="Image 1" className="slider-image"/>
+          <img src="2.jpg" alt="Image 2" className="slider-image"/>
+          <img src="3.jpg" alt="Image 3" className="slider-image"/>
+          <img src="4.jpg" alt="Image 4" className="slider-image"/>
+          <img src="5.jpg" alt="Image 5" className="slider-image"/>
+          <img src="6.jpg" alt="Image 6" className="slider-image"/>
+          <img src="7.jpg" alt="Image 7" className="slider-image"/>
+          <img src="8.jpg" alt="Image 8" className="slider-image"/>
+          <img src="9.jpg" alt="Image 9" className="slider-image"/>
+          <img src="10.jpg" alt="Image 10" className="slider-image"/>
+          <img src="11.jpg" alt="Image 11" className="slider-image"/>
+          <img src="12.jpg" alt="Image 12" className="slider-image"/>
+          <img src="13.jpg" alt="Image 13" className="slider-image"/>
+        </div>
+      </div>
     </div>
   )
 }
