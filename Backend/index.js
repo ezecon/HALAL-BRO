3fd125f62fc7a49d2e13954ab23aecd8a2926ca0
ezecon <nationhexus@gmail.com>
2024-08-20 14:26:22 +0600

    allah vorosha

--- Backend/index.js
+++ Backend/index.js
@@ -5,44 +5,51 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = 3000;
+const port =  3000; 
 
 // Middlewares
 app.use(express.json({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 // Configure CORS
 app.use(cors({
-  origin: '*', // Replace with the origin of your frontend
-  credentials: true // Allow credentials to be sent
+  origin: '*',
+  credentials: true 
 }));
-app.use(cookieParser())
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://mdeconozzama:UFyGpfTmxCEVKHrU@cluster0.7fkqi.mongodb.net/database?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URI, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => {
   console.error(err.message);
-  process.exit(1);   
+  process.exit(1);
 });
 
 // Routes
-const product = require('./routes/products.js');
+const product = require('./routes/products');
 app.use('/api/v2/products', product);
 
 const authRoutes = require('./routes/auth');
 app.use('/api/v2/auth', authRoutes);
 
-const user = require('./routes/User.js');
+const user = require('./routes/User');
 app.use('/api/v2/users', user);
 
-const cart = require('./routes/cart.js');
+const cart = require('./routes/cart');
 app.use('/api/v2/carts', cart);
 
-const order = require('./routes/order.js');
+const order = require('./routes/order');
 app.use('/api/v2/orders', order);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
